aebecbb826a2009dbad95afc93656e23ae88af4c
Duc Trung <ducdt1992@gmail.com>
2018-04-19 17:05:15 +0700

    refactor

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
 import requests
 import datetime
 
+token = '551221796:AAGFysvaN_YpOA-kA0K0GeOr-AEJp3ZZfFg'
+greet_bot = BotHandler(token)
+greetings = ('hello', 'hi', 'greetings', 'sup')
+
+now = datetime.datetime.now()
+
+
+def main():
+    new_offset = None
+    today = now.day
+    hour = now.hour
+
+
+while True:
+        greet_bot.get_updates(new_offset)
+last_update = greet_bot.get_last_update()
+last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
+if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            greet_bot.send_message(
+                last_chat_id, 'Good Morning  {}'.format(last_chat_name))
+            today += 1
+elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            greet_bot.send_message(
+                last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
+            today += 1
+elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            greet_bot.send_message(
+                last_chat_id, 'Good Evening  {}'.format(last_chat_name))
+            today += 1
+new_offset = last_update_id + 1
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
+
 
 class BotHandler:
 
@@ -26,8 +65,7 @@ def send_message(self, chat_id, text):
 
 
 def get_last_update(self):
-        get_result = self.get_updates()
-
+        et_result = self.get_updates()
 
 if len(get_result) > 0:
             last_update = get_result[-1]
@@ -35,35 +73,4 @@ if len(get_result) > 0:
             last_update = get_result[len(get_result)]
 return last_update
 
-token = '551221796:AAGFysvaN_YpOA-kA0K0GeOr-AEJp3ZZfFg'
-greet_bot = BotHandler(token)  
-greetings = ('hello', 'hi', 'greetings', 'sup')  
 
-now = datetime.datetime.now()
-
-def main():  
-    new_offset = None
-    today = now.day
-    hour = now.hour
-while True:
-        greet_bot.get_updates(new_offset)
-last_update = greet_bot.get_last_update()
-last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
-if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
-            today += 1
-elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
-            today += 1
-elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Good Evening  {}'.format(last_chat_name))
-            today += 1
-new_offset = last_update_id + 1
-if __name__ == '__main__':  
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit()
